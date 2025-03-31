36d56f98f1898eb40acd065ef015021008452517
olivier <otaveau@gmail.com>
2025-03-31 15:13:46 +0200

    simple deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,74 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import CalendarView from '/components/Calendar/CalendarView.jsx';
-import Login from '/components/Auth/Login.jsx';
-import Register from '/components/Auth/Register.jsx';
-import OwnerManagement from '/components/Manage/OwnerManagement.jsx';
-import TeamManagement from '/components/Manage/TeamManagement.jsx';
-import Navigation from '/components/Common/Navigation.jsx';
-import { AuthProvider, useAuth } from '/context/AuthContext.jsx';
-import Spinner from '/components/Common/Spinner.jsx';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-const PrivateWrapper = ({ children }) => {
-  const { state } = useAuth();
-  return state.loading ? <Spinner /> : (state.isAuthenticated ? children : <Navigate to="/login" replace />);
-};
-
-const PublicOnlyWrapper = ({ children }) => {
-  const { state } = useAuth();
-  return state.loading ? <Spinner /> : (state.isAuthenticated ? <Navigate to="/calendar" replace /> : children);
-};
-
-
-const RootRedirect = () => {
-  const { state } = useAuth();
-  return state.loading ? <Spinner /> : (
-    state.isAuthenticated ? <Navigate to="/calendar" replace /> : <Navigate to="/login" replace />
-  );
-};
-
-const AppContent = () => {
-  const { state } = useAuth();
-
-  if (state.loading) {
-    return <Spinner />;
-  }
-
-  return (
-    <BrowserRouter>
-      {state.isAuthenticated && <Navigation />}
-      <div className="flex justify-center mx-autopx-4 pt-5 transition-colors duration-200 dark:bg-gray-900 min-h-screen">
-        <Routes>
-          <Route path="/" element={<RootRedirect />} />
-          <Route path="/login" element={<PublicOnlyWrapper><Login /></PublicOnlyWrapper>} />
-          <Route path="/register" element={<PublicOnlyWrapper><Register /></PublicOnlyWrapper>} />
-          <Route path="/calendar" element={<PrivateWrapper><CalendarView /></PrivateWrapper>} />
-          <Route path="/owners" element={<PrivateWrapper><OwnerManagement /></PrivateWrapper>} />
-          <Route path="/teams" element={<PrivateWrapper><TeamManagement /></PrivateWrapper>} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-   );
-};
 
 const App = () => (
-  <AuthProvider>
-    <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <AppContent />
-  </AuthProvider>
+  <div>
+    <h1>Application Déployée avec Succès!</h1>
+  </div>
 );
 
 export default App;
